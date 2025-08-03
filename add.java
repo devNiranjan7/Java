b0fa8575a34ca46a0b23be3ab1a4a8e81f5856ee
Dev Niranjan <devniranjan0111@gmail.com>
2025-08-03 10:13:23 +0530

    added add.java

---
+++ add.java
@@ -0,0 +1,10 @@
+public class add{
+    public static void main(String[] args){
+        int num1,num2,num3,sum;
+        num1 = 2;
+        num2 = 4;
+        num3 = 7;
+        sum = num1 + num2 + num3;
+        System.out.println("The sum of numbers is : "+sum);
+    }
+}
