7a493f7bbdbe0f17edbb35abe155b1a5934e221b
Dev Niranjan <devniranjan0111@gmail.com>
2025-08-04 15:59:56 +0530

    update add.java and add calcPercent.java

--- add.java
+++ add.java
@@ -1,10 +1,10 @@
 public class add{
     public static void main(String[] args){
-        int num1,num2,num3,sum;
-        num1 = 2;
-        num2 = 4;
-        num3 = 7;
-        sum = num1 + num2 + num3;
-        System.out.println("The sum of numbers is : "+sum);
+        int num1 = 2;
+        int num2 = 4;
+        int num3 = 7;
+        int sum = num1 + num2 + num3;
+        
+        System.out.println("The sum of numbers is : " + sum);
     }
 }
